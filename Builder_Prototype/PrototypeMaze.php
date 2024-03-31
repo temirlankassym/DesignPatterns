1232ebcd7d08a5d156bbe2d80505cf2e1fd29a1d
temirlankassym <kassym22t@gmail.com>
2024-03-31 16:40:25 +0500

    Fix: prototypeMaze

--- Builder_Prototype/PrototypeMaze.php
+++ Builder_Prototype/PrototypeMaze.php
@@ -3,23 +3,29 @@
 require_once 'Maze.php';
 
 //create old game
-$maze = new Maze();
+class CloneableMazeGame{
+    public static function createMaze(){
+        $maze = new Maze();
 
-$room1 = new Room(1);
-$room2 = new Room(2);
-$door = new DoorWall($room1, $room2);
+        $room1 = new Room(1);
+        $room2 = new Room(2);
+        $door = new DoorWall($room1, $room2);
 
-$maze->addRoom($room1);
-$maze->addRoom($room2);
+        $maze->addRoom($room1);
+        $maze->addRoom($room2);
 
-$room1->setSide(DIRECTION['n'], $door);
-$room1->setSide(DIRECTION['e'], new Wall());
-//...
+        $room1->setSide(DIRECTION['n'], $door);
+        $room1->setSide(DIRECTION['e'], new Wall());
+        //...
 
+        return $maze;
+    }
+}
+
+$maze = CloneableMazeGame::createMaze();
 print_r($maze);
 
 //clone old game
 
 $copyOfMaze = clone $maze;
-
 print_r($copyOfMaze);
